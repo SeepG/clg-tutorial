93e73c29866a5e6bc6b7db57a6ee042c725fb148
SeepG <seep.gulati@estimateone.com>
2021-12-21 16:08:19 +1100

    Fixed the trimming for user input

--- lesson2-activity/Program.cs
+++ lesson2-activity/Program.cs
@@ -1,7 +1,9 @@
 ﻿
-Console.WriteLine(" ** Welcome to Mad Libs, where you spin your own mythical story **");
+Console.WriteLine("");
 
-Console.WriteLine("Let's customize your unique journey with few answers!");
+Console.WriteLine(" ** Welcome to Mad Libs, where you spin your own mythical story **\n");
+
+Console.WriteLine("Let's customize your unique journey with few answers!\n");
 
 Console.ForegroundColor = ConsoleColor.Blue;
 
@@ -10,37 +12,35 @@ Console.WriteLine("=====================================");
 // Restore the original console colors.
 Console.ResetColor();
 
-Console.WriteLine("Q. Please share your name?");
+Console.WriteLine("Q. Please share your name?\n");
+var playerName = Console.ReadLine()?.Trim();
 
-var playerName = Console.ReadLine();
+Console.WriteLine("");
 
-Console.WriteLine("Q. Please provide your your preferred way to travel from options below:");
+Console.WriteLine("Q. Please provide your your preferred way to travel from options below:\n");
 Console.WriteLine("a. Bus");
 Console.WriteLine("b. Train");
 Console.WriteLine("c. Plane");
+var preferredTransport = Console.ReadLine()?.Trim();
 
-var preferredTransport = Console.ReadLine();
+Console.WriteLine("Q. Pick your favorite mythical creature?\n");
+var mythicalCreature = Console.ReadLine()?.Trim();
 
-Console.WriteLine("Q. Pick your favorite mythical creature?");
+Console.WriteLine("Q. Pick a superpower?\n");
+var superpower = Console.ReadLine()?.Trim();
+Console.WriteLine("");
 
-var mythicalCreature = Console.ReadLine();
+Console.WriteLine("Q. Share your dream travel destination ?\n");
+var dreamTravelDestination = Console.ReadLine()?.Trim();
+Console.WriteLine("");
 
+Console.WriteLine("Q. Pick an adverb, e.g. playfully, sadly\n");
+var adverb = Console.ReadLine()?.Trim();
+Console.WriteLine("");
 
-Console.WriteLine("Q. Pick a superpower?");
-
-var superpower = Console.ReadLine();
-
-Console.WriteLine("Q. Share your dream travel destination ?");
-
-var dreamTravelDestination = Console.ReadLine();
-
-Console.WriteLine("Q. Pick an adverb, e.g. playfully, sadly");
-
-var adverb = Console.ReadLine();
-
-Console.WriteLine("Q. Pick a plural noun");
-
-var pluralNoun = Console.ReadLine();
+Console.WriteLine("Q. Pick a plural noun\n");
+var pluralNoun = Console.ReadLine()?.Trim();
+Console.WriteLine("");
 
 Console.WriteLine("=====================================");
 
@@ -48,7 +48,7 @@ Console.WriteLine("Here is your story");
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 
-Console.WriteLine($"{playerName?.Trim()} woke up in the night with a dream of a new quest to meet with {mythicalCreature?.Trim()} of { dreamTravelDestination?.Trim()}, {adverb?.Trim()} started looking at tickets for {preferredTransport?.Trim()}.");
-Console.WriteLine($"{playerName?.Trim()} was very happy to get a booking for coming weekend, when suddenly {pluralNoun?.Trim()} appeared.");
-Console.WriteLine($"{pluralNoun?.Trim()} made a screaching sound and used their {superpower?.Trim()} power.");
-Console.WriteLine($"{playerName?.Trim()} was very frightened and immediately got up from the bed, then realized that the sound was from the alarm tone.");
+Console.WriteLine($"{playerName} woke up in the night with a dream of a new quest to meet with {mythicalCreature} of { dreamTravelDestination}, {adverb} started looking at tickets for {preferredTransport}.");
+Console.WriteLine($"{playerName} was very happy to get a booking for coming weekend, when suddenly {pluralNoun} appeared.");
+Console.WriteLine($"{pluralNoun} made a screaching sound and used their {superpower} power.");
+Console.WriteLine($"{playerName} was very frightened and immediately got up from the bed, then realized that the sound was from the alarm tone.");
