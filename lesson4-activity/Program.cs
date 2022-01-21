a897703b4cfb1235309075da5c3d16ef1782dec6
SeepG <seep.gulati@estimateone.com>
2022-01-21 19:05:11 +1100

    Updated print statements

--- lesson4-activity/Program.cs
+++ lesson4-activity/Program.cs
@@ -9,7 +9,7 @@ Console.ForegroundColor = ConsoleColor.DarkCyan;
 Console.BackgroundColor = ConsoleColor.White;
 
 var luckyNumber = new Random().Next(1, 11);
-Console.WriteLine("Guess a number from 1 to 10:    ");
+Console.WriteLine("Guess a number from 1 to 10\n");
 
 var guessedNumber = 0;
 
@@ -17,38 +17,44 @@ var allowedGuesses = 3;
 var numberOfTries = 0;
 
 while (numberOfTries < allowedGuesses)
+
 {
   int.TryParse(Console.ReadLine(), out guessedNumber);
   numberOfTries++;
 
   if (guessedNumber == luckyNumber)
   {
-    Console.WriteLine($"Bingo! You guessed correctly.");
+    Console.WriteLine($"Bingo! You guessed correctly.\n");
     break;
   }
+
   else if (guessedNumber > luckyNumber)
   {
-    Console.Write($"Your guessed number is 🆙 High.");
+    Console.WriteLine($"Your guessed number is 🆙 High.\n");
   }
 
   else if (guessedNumber < luckyNumber)
   {
-    Console.WriteLine($"Your guessed number is ⬇️ Low.");
+    Console.WriteLine($"Your guessed number is ⬇️ Low.\n");
   }
 
   if (luckyNumber % 2 == 0)
   {
-    Console.WriteLine($"The lucky number is Even.");
+    Console.WriteLine($"The lucky number is Even.\n");
   }
+
   else if (luckyNumber % 2 != 0)
   {
-    Console.WriteLine($"The lucky number is Odd.");
+    Console.WriteLine($"The lucky number is Odd.\n");
   }
-  Console.WriteLine($"You have {Math.Abs(numberOfTries - allowedGuesses)} tries left");
+
+  if (numberOfTries == 2)
+  {
+    Console.WriteLine($"Try again. You have {Math.Abs(numberOfTries - allowedGuesses)} tries left\n");
+  }
 
 }
-Console.WriteLine($"Thanks for playing. We will see you next time. The luck number was: {luckyNumber}.");
 
-Console.WriteLine(" ");
+Console.WriteLine($"Thanks for playing. We will see you next time. The luck number was: {luckyNumber}.\n");
 
 Console.WriteLine("🎲 🎲 🎲 🎲");
