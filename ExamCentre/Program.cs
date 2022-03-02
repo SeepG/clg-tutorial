50432dfff933f808fdca965629272424df0ea65e
SeepG <seep.gulati@estimateone.com>
2022-03-03 10:41:27 +1100

    Removed commented code and moved to WIP.txt. Will revisit later

--- ExamCentre/Program.cs
+++ ExamCentre/Program.cs
@@ -15,9 +15,6 @@ public static void Main()
         exam1.AddStudent(students[1]);
         exam1.AddStudent(students[2]);
 
-        // List<Exam> exams = SeedExams();
-        // exams = AddStudent(exams,students);
-
         exam1.MarkPaper(3315, 45);
         exam1.MarkPaper(3316, 90);
         exam1.MarkPaper(3317, 60);
@@ -37,28 +34,6 @@ public static void Main()
         List<Student> students = new List<Student> { student1, student2, student3 };
         return students;
         }
-       /* static List<Exam> SeedExams (){
-        Exam exam1 = new Exam("Data Types");
-        Exam exam2 = new Exam("Methods");
-
-        List<Exam> exams = new List<Exam> { exam1, exam2 };
-        return exams;
-        }
-
-        static List<Exam> AddStudent(List<Exam> exams, List<Student> students){
-        var updatedExams = new List<Exam>();
-        foreach (var exam in exams)
-        {
-          var updatedExam = new Exam(exam.Subject);
-           foreach (var student in students)
-           {
-               updatedExam.AddStudent(student);
-           }
-           updatedExams.Add(updatedExam);
-        }
-
-        return updatedExams;
-    }*/
 
     }
 }
